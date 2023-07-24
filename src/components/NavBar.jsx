c408aaf9f78e9fc3e5d73a7403706609354aa83f
Takeadev <chanceconwaydev@gmail.com>
2023-07-24 17:44:25 -0400

    Create navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Loon from '../../assets/ryanlerch_Common_Loon.svg'
+
+function NavBar() {
+  return (
+    <div className='bg-cyan-200'>
+      <img className='w-20 bg-white rounded-full' src={Loon} alt='Loon' />
+    </div>
+  )
+}
+
+export default NavBar
