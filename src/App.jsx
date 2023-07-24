b099deb5464f25abc9401b0445da50bba69f775d
Takeadev <chanceconwaydev@gmail.com>
2023-07-24 16:40:25 -0400

    Add home component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
-  return <h1>Hello world!</h1>
+  return <Home />
 }
 
 export default App
