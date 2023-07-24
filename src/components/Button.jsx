515e5b2d15a6ff1ef3b83e50734da820696cec68
Takeadev <chanceconwaydev@gmail.com>
2023-07-24 17:44:17 -0400

    Change color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({ label }) {
   return (
-    <button className='text-black text-3xl bg-teal-300 rounded-full w-2/5 py-2 px-3 mr-10'>
+    <button className='text-black text-3xl bg-cyan-300 rounded-full w-2/5 py-2 px-3 mr-10'>
       {label}
     </button>
   )
