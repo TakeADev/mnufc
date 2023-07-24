09405c4e5285df2b685ad254553f99228b9da34a
Takeadev <chanceconwaydev@gmail.com>
2023-07-24 17:01:44 -0400

    Create routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
-import Home from './components/Home/Home'
+import Home from './pages/Home/Home'
+import FeedPage from './pages/FeedPage/FeedPage'
 
 function App() {
-  return <Home />
-}
+  return (<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/feed' element={<FeedPage />} />
+    </Routes>
+  </BrowserRouter>
+  )}
 
 export default App
