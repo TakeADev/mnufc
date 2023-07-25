97df99bfb1862ef4c377977f4d92c0f57f4e6e66
Takeadev <chanceconwaydev@gmail.com>
2023-07-24 21:13:26 -0400

    Start navbar design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-
-import Loon from '../../assets/ryanlerch_Common_Loon.svg'
+import { Link } from 'react-router-dom'
+import Button from './Button'
 
 function NavBar() {
   return (
-    <div className='bg-cyan-200'>
-      <img className='w-20 bg-white rounded-full' src={Loon} alt='Loon' />
+    <div className='dark:bg-gray-950 sticky top-0 flex'>
+      <div className='w-20'>
+        <Link>
+          <img
+            className=''
+            src='https://gravectory.com/wp-content/uploads/2023/03/minnesota_united_24.png'
+            alt='Loon'
+          />
+        </Link>
+      </div>
+      <div className='absolute right-0'>
+        <Button addedClasses='py-1 px-2 text-xl mx-20 my-5'>Login</Button>
+      </div>
     </div>
   )
 }
