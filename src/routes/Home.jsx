fedfdaefce6b24340d9dad72347575103c4fa400
Takeadev <chanceconwaydev@gmail.com>
2023-07-24 21:13:46 -0400

    Start structure for feed.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+const posts = [
+  { user: 'Dave', content: "Wow I can't believe the goal!!!" },
+  { user: 'Dave', content: "Wow I can't believe the goal!!!" },
+  { user: 'Dave', content: "Wow I can't believe the goal!!!" },
+  { user: 'Dave', content: "Wow I can't believe the goal!!!" },
+  { user: 'Dave', content: "Wow I can't believe the goal!!!" },
+  { user: 'Dave', content: "Wow I can't believe the goal!!!" },
+]
+
 function Home() {
-  return <div>Home Page</div>
+  return (
+    <div className='flex w-full'>
+      <div className='w-3/4 mx-auto bg-gray-800'>
+        {posts.map(({ user, content }) => {
+          return <div className='flex w-full h-10'>{user}</div>
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home
