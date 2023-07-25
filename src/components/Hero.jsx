bd1a801f6f000a8b1f297c4718c2a6b8be11b42d
Takeadev <chanceconwaydev@gmail.com>
2023-07-24 21:13:09 -0400

    Update hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,19 @@ import Button from './Button'
 
 function Hero() {
   return (
-    <div id='hero' className='w-screen h-screen bg-black flex items-center'>
+    <div
+      id='hero'
+      className='w-screen h-screen flex items-center dark:bg-gray-950'
+    >
       <div className='mx-auto w-1/2  max-w-xl'>
-        <span className='text-white text-7xl'>Minnesota United Fan Club</span>
-        <h2 className='text-white text-4xl my-10'>Swim with the Loons!</h2>
-        <Button label='Login' />
-        <Button label='Sign-Up' />
+        <span className='text-7xl'>Minnesota United Fan Club</span>
+        <h2 className='text-4xl my-10'>Swim with the Loons!</h2>
+        <Button addedClasses='text-3xl w-2/5 py-2 px-3 mr-10'>Login</Button>
+        <Button addedClasses='text-3xl w-2/5 py-2 px-3 mr-10'>Sign-Up</Button>
       </div>
       <div className='w-1/2 text-left pl-10 max-w-3xl h-screen mr-0'>
         <img
-          className='h-screen rounded-3xl absolute float-right'
+          className='rounded-3xl float-left h-5/6'
           src='https://images.unsplash.com/photo-1627955353583-5ed9f3ff1dc7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=627&q=80'
           alt='Loon-In-Water'
         />
