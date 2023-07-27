0cef272e8666ba2fe711790392c5368e4c3b4288
Takeadev <chanceconwaydev@gmail.com>
2023-07-27 17:28:46 -0400

    Create file for dummy post data

---
+++ post-data.js
@@ -0,0 +1,37 @@
+const placeholderPic =
+  'https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png'
+
+export const posts = [
+  {
+    id: 0,
+    name: 'Dave',
+    handle: '@davethedude',
+    time: '9:45pm',
+    profilePic: placeholderPic,
+    content: "Wow I can't believe the goal!!!",
+  },
+  {
+    id: 2,
+    name: 'Dave',
+    handle: '@davethedude',
+    time: '9:45pm',
+    profilePic: placeholderPic,
+    content: "Wow I can't believe the goal!!!",
+  },
+  {
+    id: 3,
+    name: 'Dave',
+    handle: '@davethedude',
+    time: '9:45pm',
+    profilePic: placeholderPic,
+    content: "Wow I can't believe the goal!!!",
+  },
+  {
+    id: 4,
+    name: 'Dave',
+    handle: '@davethedude',
+    time: '9:45pm',
+    profilePic: placeholderPic,
+    content: "Wow I can't believe the goal!!!",
+  },
+]
