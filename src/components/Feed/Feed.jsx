7e7d42427b9acef424f9348113d2d8413eb78dab
TakeADev <chanceconwaydev@gmail.com>
2023-08-03 06:11:55 -0400

    Move feed components to Feed folder.

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import PostContainer from './PostContainer'
-import PostInfoContainer from './PostInfoContainer'
-import ProfilePicBubble from './ProfilePicBubble'
-import PostInfo from './PostInfo'
-import PostContent from './PostContent'
+import PostContainer from '../Posts/PostContainer'
+import PostInfoContainer from '../Posts/PostInfoContainer'
+import ProfilePicBubble from '../ProfilePicBubble'
+import PostInfo from '../Posts/PostInfo'
+import PostContent from '../Posts/PostContent'
 
-import { posts } from '../../post-data'
+import { posts } from '../../../post-data'
 import FeedContainer from './FeedContainer'
 
 function Feed() {
