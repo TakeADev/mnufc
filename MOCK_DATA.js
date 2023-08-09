34dcd13174b13c89a0cfc3baf6a34010efb78124
TakeADev <chanceconwaydev@gmail.com>
2023-08-09 18:00:02 -0400

    Create file for mock post data

---
+++ MOCK_DATA.js
@@ -0,0 +1,389 @@
+export const dummyPosts = [
+  {
+    post_id: '5NMTC4AVSHS35VM',
+    user_id: '9p8aaj9z5gpntxiewk2',
+    content:
+      'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    timestamp: '4/24/2022',
+    likes: 395,
+  },
+  {
+    post_id: 'EB5QUL55UHUBNT3',
+    user_id: 'nf6hxew6vmhrngkrusf',
+    content:
+      'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
+    timestamp: '4/5/2022',
+    likes: 943,
+  },
+  {
+    post_id: '0KBKA8I4OVPGHWW',
+    user_id: '73nhazlkt0dlp348bgr',
+    content:
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    timestamp: '12/13/2022',
+    likes: 616,
+  },
+  {
+    post_id: 'V2BAONEJP4J7VT5',
+    user_id: 'rmgxf56sndtwpeg362c',
+    content:
+      'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.',
+    timestamp: '8/7/2022',
+    likes: 632,
+  },
+  {
+    post_id: 'K5YJFJ3ACRJO8G7',
+    user_id: 'waxvvurudqvm7u38vwv',
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.',
+    timestamp: '7/30/2022',
+    likes: 644,
+  },
+  {
+    post_id: 'JOIG7TX4NQ5US74',
+    user_id: 'mh6pvm13t2z4musli8i',
+    content: 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.',
+    timestamp: '11/23/2022',
+    likes: 264,
+  },
+  {
+    post_id: 'GBH1SXB740ZPIUZ',
+    user_id: 'w5zyejy1gb8jnisq95p',
+    content:
+      'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.',
+    timestamp: '12/8/2022',
+    likes: 857,
+  },
+  {
+    post_id: 'QHLH8A84T7GIB6S',
+    user_id: 'ktb2sc2zjac5p3ckt9h',
+    content:
+      'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.',
+    timestamp: '8/29/2022',
+    likes: 249,
+  },
+  {
+    post_id: '2E4GUPSEALM90F1',
+    user_id: 'dzgxabet5bddm8zximi',
+    content: 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    timestamp: '11/21/2022',
+    likes: 935,
+  },
+  {
+    post_id: '60PSHBUN0I0K58L',
+    user_id: '7cce2vlmv0p5m88nm83',
+    content:
+      'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+    timestamp: '1/16/2022',
+    likes: 482,
+  },
+  {
+    post_id: 'F09THV3N44SGR2L',
+    user_id: 'm95dj3z9bwjc2cxu7cu',
+    content:
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    timestamp: '8/5/2022',
+    likes: 705,
+  },
+  {
+    post_id: 'SE50Y035FOXP99E',
+    user_id: '2zbe32t3gufum6z91gj',
+    content:
+      'Phasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.',
+    timestamp: '11/25/2022',
+    likes: 24,
+  },
+  {
+    post_id: '9GO1G38GKB22J46',
+    user_id: 'i5tzzq0cchu8ejwsrxq',
+    content:
+      'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.',
+    timestamp: '3/12/2022',
+    likes: 599,
+  },
+  {
+    post_id: '0FGTOIDX9RHDQP9',
+    user_id: 'f7j5fdt5sx0h958m7q5',
+    content: 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.',
+    timestamp: '11/25/2022',
+    likes: 993,
+  },
+  {
+    post_id: 'KGA5LDR58BT4MGQ',
+    user_id: 'slg5jed6swliu5k12lk',
+    content: 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
+    timestamp: '7/29/2022',
+    likes: 392,
+  },
+  {
+    post_id: 'MPML27P7ZDDY9H5',
+    user_id: '8hesl6wnf5gokavfh4j',
+    content:
+      'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.',
+    timestamp: '12/19/2022',
+    likes: 890,
+  },
+  {
+    post_id: '1D7T9JSYATIAAWU',
+    user_id: 'pjlga8y2wtkp2do99ni',
+    content:
+      'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.',
+    timestamp: '8/27/2022',
+    likes: 589,
+  },
+  {
+    post_id: 'TJZYS76SWK7D5LR',
+    user_id: 'x3vniqefmk3736hlvuu',
+    content:
+      'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
+    timestamp: '10/10/2022',
+    likes: 372,
+  },
+  {
+    post_id: 'N04VT689GVJNM8Y',
+    user_id: 'gtpzrefzo8uu42x52ac',
+    content:
+      'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.',
+    timestamp: '4/15/2022',
+    likes: 672,
+  },
+  {
+    post_id: 'TRJ69YL25E3SQO7',
+    user_id: 'srle6trac9rk0i66soc',
+    content:
+      'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    timestamp: '11/4/2022',
+    likes: 437,
+  },
+  {
+    post_id: '8IGPN9V69VXRPJ0',
+    user_id: 'yg07oamx0cgovbnyofb',
+    content:
+      'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    timestamp: '7/15/2022',
+    likes: 368,
+  },
+  {
+    post_id: '6JTGG6JM60C96OD',
+    user_id: 'a6yis0pmwhxnerffj39',
+    content:
+      'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    timestamp: '3/10/2022',
+    likes: 582,
+  },
+  {
+    post_id: '9PNGQ5AWLK7VQN6',
+    user_id: 'm49q104eje7yr55sggt',
+    content: 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    timestamp: '12/30/2022',
+    likes: 754,
+  },
+  {
+    post_id: 'MQJVPHUYD0V9CCX',
+    user_id: '98lug5ldggcbquwsmq1',
+    content: 'Phasellus in felis. Donec semper sapien a libero. Nam dui.',
+    timestamp: '10/9/2022',
+    likes: 806,
+  },
+  {
+    post_id: 'UH9OK9C1SQHY4XE',
+    user_id: 'wiivyd2s408jt7wzun8',
+    content:
+      'Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    timestamp: '8/9/2022',
+    likes: 592,
+  },
+  {
+    post_id: 'PE6F3KDVAK8DLHH',
+    user_id: '5k3scxtq6ha178q0z1y',
+    content:
+      'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    timestamp: '8/14/2022',
+    likes: 499,
+  },
+  {
+    post_id: 'PFFZUGXANT7HJ23',
+    user_id: 'gaj8d2tl6l3fwvnnsn0',
+    content:
+      'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
+    timestamp: '11/15/2022',
+    likes: 603,
+  },
+  {
+    post_id: '3WNINLDDRFY381P',
+    user_id: 'ynzlkndcmpx3590ujnz',
+    content:
+      'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.',
+    timestamp: '1/21/2022',
+    likes: 80,
+  },
+  {
+    post_id: 'Q96QFANBWMM4WZR',
+    user_id: 'ii1ziinoy7fcs5iedge',
+    content: 'Phasellus in felis. Donec semper sapien a libero. Nam dui.',
+    timestamp: '8/31/2022',
+    likes: 377,
+  },
+  {
+    post_id: '6AQ8O1FBECPZG1N',
+    user_id: 'f58i3725axyagpp18la',
+    content: 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
+    timestamp: '6/11/2022',
+    likes: 296,
+  },
+  {
+    post_id: '6IYJS4QIHBPBT47',
+    user_id: '6zoap7ti5wwg6uhuvkx',
+    content:
+      'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    timestamp: '9/18/2022',
+    likes: 163,
+  },
+  {
+    post_id: '65S07S1I6B8TMD5',
+    user_id: 'sbfshpqmfa3inv0t27y',
+    content:
+      'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
+    timestamp: '6/29/2022',
+    likes: 668,
+  },
+  {
+    post_id: 'Y61572UB9GLZ63Q',
+    user_id: '98gfn36iomwi4exupv2',
+    content:
+      'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.',
+    timestamp: '8/14/2022',
+    likes: 640,
+  },
+  {
+    post_id: 'EE521P1HYLEU5EP',
+    user_id: 'yx6ov7hp1gq8zl28z6s',
+    content: 'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.',
+    timestamp: '9/28/2022',
+    likes: 94,
+  },
+  {
+    post_id: '6S492RPMP2JB6U5',
+    user_id: 'q9xbk0bewr4g65q87cb',
+    content:
+      'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.',
+    timestamp: '2/4/2022',
+    likes: 770,
+  },
+  {
+    post_id: 'QCVFYMBFDC4RRTS',
+    user_id: 'dbgwek9r4cwj52wxjnf',
+    content:
+      'Phasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.',
+    timestamp: '7/18/2022',
+    likes: 760,
+  },
+  {
+    post_id: '8JETKBJH8Z5AXGR',
+    user_id: '1f1dyyw17j6rgu1xsb7',
+    content:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.\n\nVestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.',
+    timestamp: '12/20/2022',
+    likes: 279,
+  },
+  {
+    post_id: 'E7D4DFAYFDV9UZE',
+    user_id: 'bjnkw5m1ryx02o1p5ae',
+    content: 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    timestamp: '3/1/2022',
+    likes: 320,
+  },
+  {
+    post_id: 'VPVIWB3UFQPJ8Z2',
+    user_id: 'vdxyrfqrfwwlhdoe11l',
+    content:
+      'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.',
+    timestamp: '7/17/2022',
+    likes: 13,
+  },
+  {
+    post_id: 'V4WL8SOK02OL7UG',
+    user_id: 'h59dm9234k8cz5xmt2v',
+    content:
+      'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+    timestamp: '3/15/2022',
+    likes: 132,
+  },
+  {
+    post_id: 'J9VH7MLNHQ38PYJ',
+    user_id: '7ja82owibg4vwu1t1da',
+    content:
+      'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.',
+    timestamp: '4/30/2022',
+    likes: 258,
+  },
+  {
+    post_id: 'MU8IXMVAD5XO7Z4',
+    user_id: 'zm4v2vzrgdfvzmazra6',
+    content:
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    timestamp: '6/5/2022',
+    likes: 430,
+  },
+  {
+    post_id: 'WOR75Q4VFZM6JY2',
+    user_id: '6fb2i9antcxt5ecd40d',
+    content:
+      'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
+    timestamp: '4/13/2022',
+    likes: 581,
+  },
+  {
+    post_id: 'S27HIKIWA8QABTC',
+    user_id: 'gp3z174dqdihxmnsbya',
+    content:
+      'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.',
+    timestamp: '5/6/2022',
+    likes: 272,
+  },
+  {
+    post_id: 'C6NVEBA82F3F1JT',
+    user_id: 'vu3vp8cxjox0az0sy6j',
+    content:
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+    timestamp: '10/3/2022',
+    likes: 374,
+  },
+  {
+    post_id: 'V5LL65NMKFHZAYN',
+    user_id: '6nk0nb2dknxn44uhnqy',
+    content:
+      'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.',
+    timestamp: '4/8/2022',
+    likes: 961,
+  },
+  {
+    post_id: 'RVWE8KH2AU59L58',
+    user_id: '8s3659kt6rl4i7f78p5',
+    content:
+      'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.',
+    timestamp: '5/16/2022',
+    likes: 740,
+  },
+  {
+    post_id: 'XD0TG5ZB7FB59LM',
+    user_id: 'iz4kcgm7cqktxghqjyp',
+    content:
+      'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.',
+    timestamp: '6/23/2022',
+    likes: 870,
+  },
+  {
+    post_id: 'WI6CKW4559B5IXN',
+    user_id: '90ng99fcst0zk847lhb',
+    content: 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.',
+    timestamp: '5/6/2022',
+    likes: 570,
+  },
+  {
+    post_id: 'WC43MR1O9WZI8K1',
+    user_id: 'tat5afj4gzgbgt97mt7',
+    content: 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
+    timestamp: '2/14/2022',
+    likes: 945,
+  },
+]
