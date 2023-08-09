44a941b3c6135f1a3c7f764d856f5880741f8245
TakeADev <chanceconwaydev@gmail.com>
2023-08-09 14:33:34 -0400

    Change initial state to null

--- src/contexts/User.jsx
+++ src/contexts/User.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 export const UserContext = createContext({ currentUser: null, setCurrentUser: () => {} })
 
 function UserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState({})
+  const [currentUser, setCurrentUser] = useState(null)
 
   const value = {
     currentUser,
