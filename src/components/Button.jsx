ff0bfcfa0a558e48555bd736fed0c8efc29cc9de
TakeADev <chanceconwaydev@gmail.com>
2023-08-09 14:31:55 -0400

    Add type prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Button({ addedClasses, onClick, children }) {
+function Button({ type, addedClasses, onClick, children }) {
   return (
-    <button className={`text-black bg-cyan-300 rounded-full ` + addedClasses} onClick={onClick}>
+    <button type={type} className={`text-black bg-cyan-300 rounded-full ` + addedClasses} onClick={onClick}>
       {children}
     </button>
   )
