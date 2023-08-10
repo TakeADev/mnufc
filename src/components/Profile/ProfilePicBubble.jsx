3ebd5f1e0b83bbd3208b2de37e8f8163c272512c
TakeADev <chanceconwaydev@gmail.com>
2023-08-10 14:19:56 -0400

    Link to direct placeholder pfp source

--- src/components/Profile/ProfilePicBubble.jsx
+++ src/components/Profile/ProfilePicBubble.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function ProfilePicBubble({ profilePic }) {
+function ProfilePicBubble() {
   return (
     <img
-      src={profilePic}
+      src='https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png'
       alt='pfp'
       className='rounded-full w-10 mx-5 mt-5 h-10'
     />
