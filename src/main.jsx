4d492428941a5363c409f551b7fe5d857675821b
TakeADev <chanceconwaydev@gmail.com>
2023-08-10 11:20:25 -0400

    Include UserPosts Context Provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 import UserProvider from './contexts/User.jsx'
+import UserPostsProvider from './contexts/UserPosts.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <UserPostsProvider>
+          <App />
+        </UserPostsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
