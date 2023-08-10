3932239abd2c7e7824e12c946ce180b21a0ffed4
TakeADev <chanceconwaydev@gmail.com>
2023-08-10 14:33:41 -0400

    Update post info to display correct information.

--- src/components/Posts/PostInfo.jsx
+++ src/components/Posts/PostInfo.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function PostInfo({ post }) {
-  const { name, user_id, timestamp } = post
+  const { name, username, timestamp } = post
   return (
     <div className='w-max mt-5 max-h-10 mb-0'>
       <span className=''>
-        <b>User</b>
+        <b>{name}</b>
       </span>
-      <span className='ml-3 text-gray-500'>{user_id}</span>
+      <span className='ml-3 text-gray-500'>@{username}</span>
       <span className='ml-3 text-gray-500'>{timestamp}</span>
     </div>
   )
