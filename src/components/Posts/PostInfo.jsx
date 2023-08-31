878c2a160ff228f1ade0d79cf617495c86264569
TakeADev <chanceconwaydev@gmail.com>
2023-08-31 15:03:47 -0400

    Post info now gets displayName from userdoc

--- src/components/Posts/PostInfo.jsx
+++ src/components/Posts/PostInfo.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getUserDocFromUid } from '../../utils/firebase/firebase-config'
 
 function PostInfo({ post }) {
-  const { name, username, timestamp } = post
-  return (
-    <div className='w-max mt-5 max-h-10 mb-0'>
-      <span className=''>
-        <b>{name}</b>
-      </span>
-      <span className='ml-3 text-gray-500 text-sm'>@{username}</span>
-      <span className='ml-3 text-gray-500 text-xs'>{timestamp}</span>
-    </div>
-  )
+  const { username, timestamp, uid } = post
+  const [postUser, setPostUser] = useState(null)
+
+  useEffect(() => {
+    getUserDocFromUid(uid).then((snap) => {
+      setPostUser(snap)
+    })
+  }, [])
+
+  if (postUser) {
+    return (
+      <div className='w-max mt-5 max-h-10 mb-0'>
+        <span className=''>
+          <b>{postUser.displayName}</b>
+        </span>
+        <span className='ml-3 text-gray-500 text-sm'>@{username}</span>
+        <span className='ml-3 text-gray-500 text-xs'>{timestamp}</span>
+      </div>
+    )
+  } else {
+    return (
+      <div className='w-max mt-5 max-h-10 mb-0'>
+        <span className=''> </span>
+        <span className='ml-3 text-gray-500 text-sm'>@{username}</span>
+        <span className='ml-3 text-gray-500 text-xs'>{timestamp}</span>
+      </div>
+    )
+  }
 }
 
 export default PostInfo
