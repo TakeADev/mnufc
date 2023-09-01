cabbcf184caef14e098845f526f24152fd9b28aa
TakeADev <chanceconwaydev@gmail.com>
2023-09-01 09:22:33 -0400

    Adds loading spinner to feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,34 +1,38 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import PostContainer from '../Posts/PostContainer'
 import PostInfoContainer from '../Posts/PostInfoContainer'
 import ProfilePicBubble from '../Profile/ProfilePicBubble'
 import PostInfo from '../Posts/PostInfo'
 import PostContent from '../Posts/PostContent'
 import FeedContainer from './FeedContainer'
+import CreatePost from '../CreatePost/CreatePost'
+import LoadingSpinner from '../LoadingSpinner'
 
 import { UserPostsContext } from '../../contexts/UserPosts'
-import CreatePost from '../CreatePost/CreatePost'
+import { FeedContext } from '../../contexts/FeedContext'
 
 function Feed() {
   const { userPosts } = useContext(UserPostsContext)
+  const { isLoading } = useContext(FeedContext)
 
   return (
     <FeedContainer>
       <CreatePost />
-      {userPosts &&
+      {userPosts ? (
         userPosts.map((post) => {
           return (
-            <PostContainer key={post.postId}>
+            <PostContainer key={post.postId} isLoading={isLoading}>
               <PostInfoContainer>
-                <ProfilePicBubble profilePic={post.profilePic} />
-
+                <ProfilePicBubble profilePic={post.profilePic} addedClasses='mx-5' />
                 <PostInfo post={post} />
               </PostInfoContainer>
-
               <PostContent content={post.content} />
             </PostContainer>
           )
-        })}
+        })
+      ) : (
+        <LoadingSpinner />
+      )}
     </FeedContainer>
   )
 }
