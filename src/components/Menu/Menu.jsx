0f42bb9b7a5673356ce953d5dd99c28c829bd644
TakeADev <chanceconwaydev@gmail.com>
2023-09-01 14:57:22 -0400

    Create menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+
+import { signOutUser } from '../../utils/firebase/firebase-config'
+
+import { MenuContext } from '../../contexts/MenuContext'
+
+import Button from '../Button'
+import MenuItem from './MenuItem'
+import MenuHeader from './MenuHeader'
+import ProfilePicBubble from '../Profile/ProfilePicBubble'
+
+import '../../styles/animations.scss'
+
+function Menu() {
+  const { isOpen, setIsOpen } = useContext(MenuContext)
+
+  const menuClickHandler = (e) => {
+    e.preventDefault()
+    setIsOpen(!isOpen)
+  }
+
+  if (isOpen) {
+    return (
+      <>
+        <div className='bg-gray-500 h-screen absolute w-full opacity-50' onClick={menuClickHandler}></div>
+        <div className='absolute right-0 flex-col bg-slate-900 rounded-xl w-1/2 md:w-2/5 lg:w-1/3 xl:w-1/4 h-screen slide-from-right'>
+          <MenuHeader onClick={menuClickHandler} />
+          <MenuItem linkPath='/' title='Feed' />
+          <MenuItem linkPath='/profile/test' title='Profile' />
+          <Button
+            addedClasses='py-1 px-1 text-lg my-5 w-4/5 mx-10 bg-slate-950 text-white border border-white'
+            onClick={signOutUser}
+          >
+            Sign Out
+          </Button>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <div className='absolute right-5'>
+        <ProfilePicBubble addedClasses='w-10 h-10' onClick={menuClickHandler} />
+      </div>
+    )
+  }
+}
+
+export default Menu
