df0bcd21a8aafe48d78ce4fb56db5af6d7528d4e
TakeADev <chanceconwaydev@gmail.com>
2023-09-01 12:17:56 -0400

    Includes addedClasses to ProfilePicBubble classes

--- src/components/Profile/ProfilePicBubble.jsx
+++ src/components/Profile/ProfilePicBubble.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function ProfilePicBubble() {
+function ProfilePicBubble({ addedClasses }) {
   return (
     <img
       src='https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png'
       alt='pfp'
-      className='rounded-full w-10 mx-5 mt-5 h-10'
+      className={`rounded-full w-10 mt-5 h-10 ${addedClasses}`}
     />
   )
 }
