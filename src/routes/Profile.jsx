6398ea037f9019c7a15f98d92e05b7c97fd49e6f
TakeADev <chanceconwaydev@gmail.com>
2023-09-01 12:18:24 -0400

    Gets profile param from URL

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../contexts/User'
+import { useParams } from 'react-router-dom'
 
 function Profile() {
   const currentUser = useContext(UserContext)
+  const username = useParams().username
+  console.log(username)
   return <div>Test</div>
 }
 
