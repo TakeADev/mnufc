1ac862ddcc6e12a1521960306c3ce5788f195c09
TakeADev <chanceconwaydev@gmail.com>
2023-09-01 14:56:05 -0400

    Remove default img size

--- src/components/Profile/ProfilePicBubble.jsx
+++ src/components/Profile/ProfilePicBubble.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function ProfilePicBubble({ addedClasses }) {
+function ProfilePicBubble({ addedClasses, onClick }) {
   return (
     <img
       src='https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png'
       alt='pfp'
-      className={`rounded-full w-10 mt-5 h-10 ${addedClasses}`}
+      className={`rounded-full mt-5 ${addedClasses}`}
+      onClick={onClick}
     />
   )
 }
