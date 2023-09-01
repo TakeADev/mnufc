e3650f25c580f5a5f900ba124c25d2daa22647bf
TakeADev <chanceconwaydev@gmail.com>
2023-09-01 14:56:39 -0400

    Menu is closed on render

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,8 +1,15 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../contexts/User'
+import React, { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
+import { UserContext } from '../contexts/User'
+import { MenuContext } from '../contexts/MenuContext'
+
 function Profile() {
+  const { setIsOpen } = useContext(MenuContext)
+
+  useEffect(() => {
+    setIsOpen(false)
+  }, [])
   const currentUser = useContext(UserContext)
   const username = useParams().username
   console.log(username)
