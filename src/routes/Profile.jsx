126a571e20f47eb5c6bd81c8a6f21297aa320116
Takeadev <chanceconwaydev@gmail.com>
2023-09-05 13:37:31 -0400

    Profile fetches and displays data based on route

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-import { getUserPostsByUsername } from '../utils/firebase/firebase-config'
+import { getUserDocFromUsername, getUserPostsByUsername } from '../utils/firebase/firebase-config'
 
-import { UserContext } from '../contexts/User'
 import { MenuContext } from '../contexts/MenuContext'
 import { FeedContext } from '../contexts/FeedContext'
+import { UserContext } from '../contexts/User'
 
 import ProfilePicBubble from '../components/Profile/ProfilePicBubble'
 import FeedContainer from '../components/Feed/FeedContainer'
@@ -14,69 +14,86 @@ import PostContainer from '../components/Posts/PostContainer'
 import PostInfoContainer from '../components/Posts/PostInfoContainer'
 import PostInfo from '../components/Posts/PostInfo'
 import PostContent from '../components/Posts/PostContent'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 function Profile() {
   const { setIsOpen } = useContext(MenuContext)
   const { isLoading } = useContext(FeedContext)
+  const { currentAuthUser } = useContext(UserContext)
+
+  const [profileUserDoc, setProfileUserDoc] = useState(null)
+
+  const username = useParams().username.toLowerCase()
+  const profilePosts = getUserPostsByUsername(username)
+  const userProfile = getUserDocFromUsername(username)
 
   useEffect(() => {
     setIsOpen(false)
-  }, [])
+  }, [username])
 
-  const username = useParams().username
-  const profilePosts = getUserPostsByUsername(username)
+  useEffect(() => {
+    userProfile.then((user) => {
+      setProfileUserDoc(user)
+    })
+  }, [username])
 
-  return (
-    <FeedContainer>
-      <div>
-        <img src='https://www.aashe.org/wp-content/uploads/2018/02/Placeholder-Banner.png' alt='' />
-      </div>
-      <div className='w-full flex-col -mt-20'>
-        <div className='flex'>
-          <div className='w-3/4'>
-            <ProfilePicBubble addedClasses='w-32 h-32 ml-5' />
+  if (profileUserDoc) {
+    return (
+      <FeedContainer>
+        <div>
+          <img src='https://www.aashe.org/wp-content/uploads/2018/02/Placeholder-Banner.png' alt='' />
+        </div>
+        <div className='w-full flex-col -mt-20'>
+          <div className='flex'>
+            <div className='w-3/4'>
+              <ProfilePicBubble addedClasses='w-32 h-32 ml-5' />
+            </div>
+            {currentAuthUser.uid === profileUserDoc.uid && (
+              <div className='w-2-5'>
+                <Button addedClasses='right-0 bg-slate-950 text-white border border-white px-5 h-10 mt-24 text-sm'>
+                  <b>Edit Profile</b>
+                </Button>
+              </div>
+            )}
           </div>
-          <div className='w-2-5'>
-            <Button addedClasses='right-0 bg-slate-950 text-white border border-white px-5 h-10 mt-24 text-sm'>
-              <b>Edit Profile</b>
-            </Button>
+          <div className='ml-5 mt-5 flex-col'>
+            <div>
+              <span className='text-xl'>
+                <b>{profileUserDoc.displayName}</b>
+              </span>
+            </div>
+            <div>
+              <span className='text-md'>@{profileUserDoc.username}</span>
+            </div>
           </div>
         </div>
-        <div className='ml-5 mt-5 flex-col'>
-          <div>
-            <span className='text-xl'>
-              <b>Chance</b>
-            </span>
-          </div>
-          <div>
-            <span className='text-md'>@takeachance</span>
-          </div>
+        <div className='text-center text-lg my-5'>
+          <span>
+            <u>
+              <b>Posts</b>
+            </u>
+          </span>
         </div>
-      </div>
-      <div className='text-center text-lg my-5'>
-        <span>
-          <u>
-            <b>Posts</b>
-          </u>
-        </span>
-      </div>
-      {profilePosts ? (
-        profilePosts.map((post) => {
-          return (
-            <PostContainer key={post.postId} isLoading={isLoading}>
-              <PostInfoContainer>
-                <ProfilePicBubble profilePic={post.profilePic} addedClasses='mx-5 h-8 w-8' />
-                <PostInfo post={post} />
-              </PostInfoContainer>
-              <PostContent content={post.content} />
-            </PostContainer>
-          )
-        })
-      ) : (
-        <LoadingSpinner />
-      )}
-    </FeedContainer>
-  )
+        {profilePosts ? (
+          profilePosts.map((post) => {
+            return (
+              <PostContainer key={post.postId} isLoading={isLoading}>
+                <PostInfoContainer>
+                  <ProfilePicBubble profilePic={post.profilePic} addedClasses='mx-5 h-8 w-8' />
+                  <PostInfo post={post} />
+                </PostInfoContainer>
+                <PostContent content={post.content} />
+              </PostContainer>
+            )
+          })
+        ) : (
+          <LoadingSpinner />
+        )}
+      </FeedContainer>
+    )
+  } else {
+    return <LoadingSpinner />
+  }
 }
 
 export default Profile
