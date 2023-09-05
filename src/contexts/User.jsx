c7428560b57e6a55d86706dafc54141cfaac161d
Takeadev <chanceconwaydev@gmail.com>
2023-09-05 09:47:19 -0400

    Create context for current user document

--- src/contexts/User.jsx
+++ src/contexts/User.jsx
@@ -1,13 +1,21 @@
 import React, { createContext, useState } from 'react'
 
-export const UserContext = createContext({ currentAuthUser: null, setCurrentAuthUser: () => {} })
+export const UserContext = createContext({
+  currentAuthUser: null,
+  setCurrentAuthUser: () => {},
+  currentUserDoc: null,
+  setCurrentUserDoc: () => {},
+})
 
 function UserProvider({ children }) {
   const [currentAuthUser, setCurrentAuthUser] = useState(null)
+  const [currentUserDoc, setCurrentUserDoc] = useState(null)
 
   const value = {
     currentAuthUser,
     setCurrentAuthUser,
+    currentUserDoc,
+    setCurrentUserDoc,
   }
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
