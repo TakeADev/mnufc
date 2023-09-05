b452c1f7fa5bcbf0266755c62cc52aeaffab9af6
Takeadev <chanceconwaydev@gmail.com>
2023-09-05 09:48:26 -0400

    Shows displayname from current user document cntxt

--- src/components/Menu/MenuHeader.jsx
+++ src/components/Menu/MenuHeader.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react'
 
+import { UserContext } from '../../contexts/User'
+
 import ProfilePicBubble from '../Profile/ProfilePicBubble'
 
 function MenuHeader() {
+  const { currentUserDoc } = useContext(UserContext)
   return (
     <div className='flex ml-5 pl-0'>
       <ProfilePicBubble addedClasses='w-10 h-10' />
-      <span className='w-3/4 mt-7 ml-3'>User</span>
+      <span className='w-3/4 mt-7 ml-3'>
+        <b>{currentUserDoc.displayName}</b>
+      </span>
     </div>
   )
 }
