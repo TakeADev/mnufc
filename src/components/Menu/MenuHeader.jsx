2063758e441dce44f8f35f0f1c4f917405b66f84
Takeadev <chanceconwaydev@gmail.com>
2023-09-05 14:24:12 -0400

    Menu header now links to current user profile

--- src/components/Menu/MenuHeader.jsx
+++ src/components/Menu/MenuHeader.jsx
@@ -3,16 +3,19 @@ import React, { useContext } from 'react'
 import { UserContext } from '../../contexts/User'
 
 import ProfilePicBubble from '../Profile/ProfilePicBubble'
+import { Link } from 'react-router-dom'
 
 function MenuHeader() {
   const { currentUserDoc } = useContext(UserContext)
   return (
-    <div className='flex ml-5 pl-0'>
-      <ProfilePicBubble addedClasses='w-10 h-10' />
-      <span className='w-3/4 mt-7 ml-3'>
-        <b>{currentUserDoc.displayName}</b>
-      </span>
-    </div>
+    <Link to={`/${currentUserDoc.username}`}>
+      <div className='flex ml-5 pl-0'>
+        <ProfilePicBubble addedClasses='w-10 h-10' />
+        <span className='w-3/4 mt-7 ml-3'>
+          <b>{currentUserDoc.displayName}</b>
+        </span>
+      </div>
+    </Link>
   )
 }
 
