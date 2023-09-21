9725e245788a3f80f7bcd461278d24eba65ca1c6
TakeADev <chanceconwaydev@gmail.com>
2023-09-21 06:50:34 -0400

    Fix rounding

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,17 +10,13 @@ function Modal({ children }) {
     <div className=''>
       {modalIsOpen && (
         <div className={`mt-28 fixed mx-auto z-50 h-screen`}>
-          <div className='bg-slate-900 h-3/4 w-3/5 lg:w-1/2 xl:w-1/3 z-0 mx-auto flex-row rounded-3xl'>
+          <div className='bg-slate-900 h-3/4 w-3/5 lg:w-1/2 xl:w-1/3 z-0 mx-auto flex-row rounded-xl'>
             <EditProfileForm />
           </div>
         </div>
       )}
 
-      <div
-        className={`${
-          modalIsOpen && 'opacity-40 bg-gray-500 '
-        }w-full h-full -z-10`}
-      >
+      <div className={`${modalIsOpen && 'opacity-40 bg-gray-500 '}w-full h-full -z-10`}>
         {children}
       </div>
     </div>
