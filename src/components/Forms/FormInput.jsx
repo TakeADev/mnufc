a5f78b275fb4be2b43bf518bc4d94c8b8fc347d7
TakeADev <chanceconwaydev@gmail.com>
2023-09-21 06:09:53 -0400

    Create component for form inputs

---
+++ src/components/Forms/FormInput.jsx
@@ -0,0 +1,16 @@
+const FormInput = ({ value, name, onChange, label }) => {
+  return (
+    <div className='mt-5 mx-auto text-center'>
+      <label className='absolute text-gray-400 ml-2'>{label}</label>
+      <input
+        type='text'
+        className='w-11/12 h-14 bg-slate-900 border border-white rounded-md focus:border-cyan-300 focus:outline-none focus:ring-0 pl-2 pt-5'
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
+
+export default FormInput
