3462a3994ec14c69590c8e9e1c821c3c51846725
TakeADev <chanceconwaydev@gmail.com>
2023-09-22 06:58:44 -0400

    Fix border color

--- src/components/Feed/FeedContainer.jsx
+++ src/components/Feed/FeedContainer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function FeedContainer({ children }) {
   return (
     <div className='flex w-full'>
-      <div className='lg:w-3/5 max-w-3xl mx-auto border-l border-r'>{children}</div>
+      <div className='lg:w-3/5 max-w-3xl mx-auto border-l border-r border-slate-700'>
+        {children}
+      </div>
     </div>
   )
 }
