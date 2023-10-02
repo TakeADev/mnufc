ec29a7c88be413c57d7bb17dc78bdde4ff136034
TakeADev <chanceconwaydev@gmail.com>
2023-10-02 11:41:16 -0400

    Remove unused import

--- src/components/Posts/PostInfo.tsx
+++ src/components/Posts/PostInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { getUserDocFromUid } from '../../utils/firebase/firebase-config'
