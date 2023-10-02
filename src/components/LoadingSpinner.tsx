f1dbeefeb44f43b7d18a008751f35e425f391e79
TakeADev <chanceconwaydev@gmail.com>
2023-10-02 11:48:55 -0400

    Add interface for LoadingSpinner props

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
+import { FunctionComponent } from 'react'
 import { ReactComponent as Spinner } from '../assets/loading-spinner.svg'
 
-function LoadingSpinner({ addedClasses }) {
+interface ILoadingSpinnerProps {
+  addedClasses: string
+}
+
+const LoadingSpinner: FunctionComponent<ILoadingSpinnerProps> = ({ addedClasses }) => {
   return (
     <div className={`w-1 mx-auto ${addedClasses}`}>
       <Spinner />
