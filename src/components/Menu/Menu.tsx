e3832609237cb34d7554bd2f790efb41f8701d21
TakeADev <chanceconwaydev@gmail.com>
2023-10-02 12:22:22 -0400

    Add currentUserDoc dependency for menu component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -11,6 +11,7 @@ import MenuHeader from './MenuHeader'
 import ProfilePicBubble from '../Profile/ProfilePicBubble'
 
 import '../../styles/animations.scss'
+import LoadingSpinner from '../LoadingSpinner'
 
 function Menu() {
   const { isOpen, setIsOpen } = useContext(MenuContext)
@@ -20,34 +21,39 @@ function Menu() {
     setIsOpen(!isOpen)
   }
 
-  if (isOpen) {
-    return (
-      <>
-        <div
-          className='bg-gray-500 h-screen absolute w-full opacity-50'
-          onClick={menuClickHandler}
-        ></div>
-        <div className='absolute right-0 flex-col bg-slate-900 rounded-xl w-1/2 md:w-2/5 lg:w-1/3 xl:w-1/4 h-screen slide-from-right'>
-          <MenuHeader onClick={menuClickHandler} />
-          <MenuItem linkPath='/' title='Feed' />
-          <MenuItem linkPath={`/${currentUserDoc.username}`} title='Profile' />
-          <Button
-            addedClasses='py-1 px-1 text-lg my-5 w-4/5 mx-10 bg-slate-900 text-white border border-white hover:bg-gray-800'
-            onClick={signOutUser}
-            type='button'
-          >
-            Sign Out
-          </Button>
+  if (currentUserDoc) {
+    if (isOpen) {
+      return (
+        <>
+          <div
+            className='bg-gray-500 h-screen absolute w-full opacity-50'
+            onClick={menuClickHandler}
+          ></div>
+          <div className='absolute right-0 flex-col bg-slate-900 rounded-xl w-1/2 md:w-2/5 lg:w-1/3 xl:w-1/4 h-screen slide-from-right'>
+            <MenuHeader onClick={menuClickHandler} />
+            <MenuItem linkPath='/' title='Feed' />
+            <MenuItem linkPath={`/${currentUserDoc.username}`} title='Profile' />
+            <Button
+              addedClasses='py-1 px-1 text-lg my-5 w-4/5 mx-10 bg-slate-900 text-white border border-white hover:bg-gray-800'
+              onClick={signOutUser}
+              type='button'
+            >
+              Sign Out
+            </Button>
+          </div>
+        </>
+      )
+    } else {
+      return (
+        <div className='absolute right-5'>
+          <ProfilePicBubble
+            addedClasses='w-10 h-10 cursor-pointer mt-5'
+            onClick={menuClickHandler}
+          />
         </div>
-      </>
-    )
-  } else {
-    return (
-      <div className='absolute right-5'>
-        <ProfilePicBubble addedClasses='w-10 h-10 cursor-pointer mt-5' onClick={menuClickHandler} />
-      </div>
-    )
-  }
+      )
+    }
+  } else return <LoadingSpinner addedClasses='absolute right-10 mt-8' />
 }
 
 export default Menu
