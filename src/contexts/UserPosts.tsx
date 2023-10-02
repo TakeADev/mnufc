731579fb29c7610c2dd0609add6179872b94c0ca
TakeADev <chanceconwaydev@gmail.com>
2023-10-02 11:39:42 -0400

    Create interface for userPost

--- src/contexts/UserPosts.tsx
+++ src/contexts/UserPosts.tsx
@@ -3,9 +3,21 @@ import { createUserPost } from '../utils/firebase/firebase-config'
 
 import firebase from 'firebase/compat/app'
 
+interface IUserPost {
+  content: String
+  displayName: String
+  likes: number
+  postId: String
+  replies: Array<IUserPost> | null
+  replyTo: String | false
+  timestamp: Date
+  uid: String
+  username: String
+}
+
 interface IUserPostsContext {
-  userPosts: Array<Object> | null
-  setUserPosts: Dispatch<SetStateAction<Array<Object>>>
+  userPosts: Array<IUserPost> | null
+  setUserPosts: Dispatch<SetStateAction<Array<IUserPost>>>
   createNewUserPost: (currentAuthUser: firebase.User, postContent: any, replyTo: any) => void
 }
 
