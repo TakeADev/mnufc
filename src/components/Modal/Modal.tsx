a9e22ca948e851ebea4a2bdb17271d1621f6f6f3
TakeADev <chanceconwaydev@gmail.com>
2023-11-13 14:47:52 -0500

    Add loginWarningModal to modal types

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,9 +4,10 @@ import EditProfileForm from '../Forms/EditProfileForm'
 import { ModalContext } from '../../contexts/ModalContext'
 import CreateReplyModal from './CreateReplyModal'
 import { IUserPost, UserPostsContext } from '../../contexts/UserPosts'
+import LoginWarningModal from './LoginWarningModal'
 
 function Modal({ children }) {
-  const { modalIsOpen, modalType, replyModalPostId } = useContext(ModalContext)
+  const { modalIsOpen, modalType, replyModalPostId, setModalIsOpen } = useContext(ModalContext)
   const { userPosts } = useContext(UserPostsContext)
 
   const replyModalPost: IUserPost =
@@ -15,17 +16,18 @@ function Modal({ children }) {
   return (
     <div>
       {modalIsOpen && (
-        <div className={`mt-28 fixed mx-auto w-full z-50 h-screen `}>
-          <div className='bg-slate-950 w-3/5 lg:w-1/2 max-w-xl z-0 mx-auto rounded-xl'>
+        <div className={`flex fixed mx-auto w-full z-50 h-screen items-center`}>
+          <div className='bg-slate-950 w-3/5 lg:w-1/2 max-w-xl z-10 mx-auto rounded-xl'>
             {modalType == 'editProfile' && <EditProfileForm />}
             {modalType == 'createPostReply' && (
               <div className='py-10'>{userPosts && <CreateReplyModal post={replyModalPost} />}</div>
             )}
+            {modalType == 'loginWarning' && <LoginWarningModal />}
           </div>
         </div>
       )}
 
-      <div className={`${modalIsOpen && 'opacity-40 bg-gray-500 '}w-full h-full -z-10`}>
+      <div className={`${modalIsOpen && 'opacity-40 bg-gray-500 '}w-full h-full z-10`}>
         {children}
       </div>
     </div>
