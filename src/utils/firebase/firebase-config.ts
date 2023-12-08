05c1457ba8753464e60677b5f497a19eb7b88c29
TakeADev <chanceconwaydev@gmail.com>
2023-12-08 16:49:49 -0500

    Uploading pfp updates userDoc w/ URL

--- src/utils/firebase/firebase-config.ts
+++ src/utils/firebase/firebase-config.ts
@@ -24,7 +24,7 @@ import {
   arrayRemove,
 } from 'firebase/firestore'
 
-import { getStorage, ref, uploadBytes } from 'firebase/storage'
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
 
 import { IUserPost } from '../../contexts/UserPosts'
 import { ICurrentUserDoc } from '../../contexts/User'
@@ -71,6 +71,8 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInfo) => {
     email: userAuth.user.email,
     likedPosts: [],
     location: '',
+    profilePic:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQicasXh3n4zKZPXpmQeWl1p2oXJJuaaRkskbnjOPaZ7SiM3CqvMJpp5WPwYBsxB3aW-eA&usqp=CAU',
     reposts: [],
     uid: userAuth.user.uid,
     username: additionalInfo.username.toLowerCase(),
@@ -294,10 +296,12 @@ export const toggleRepost = async (repostPost: IUserPost) => {
 //-----------------------------STORAGE--------------------------------------
 
 export const uploadProfilePicture = (blob: Blob, currentUserDoc: ICurrentUserDoc) => {
-  const pfpRef = ref(storage, currentUserDoc.username + `/profilePic`)
+  const pfpRef = ref(storage, currentUserDoc.username + `/profilePics/pfp-` + Date.now())
 
   uploadBytes(pfpRef, blob).then((snapshot) => {
-    console.log('Uploaded pfp!' + snapshot)
+    getDownloadURL(pfpRef).then((res) => {
+      updateDoc(doc(db, 'users', currentUserDoc.uid.toString()), { profilePic: res })
+    })
   })
 
   console.log(pfpRef.fullPath)
