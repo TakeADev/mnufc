cbb38f8e8a67654043d36eafef0b255a9b19c602
TakeADev <chanceconwaydev@gmail.com>
2023-12-15 16:48:48 -0500

    Fix styling

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -61,7 +61,7 @@ function Profile() {
             <span
               className={`${
                 tab === 'photos' && 'underline'
-              } underline-offset-8 decoration-cyan-500 hover:cursor-pointer`}
+              } underline-offset-8 decoration-cyan-500 hover:cursor-pointer pb-5 px-2`}
               onClick={photosTabClickHandler}
             >
               <b>Photos</b>
