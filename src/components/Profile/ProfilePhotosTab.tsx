77c77fcb8d7a2b0fdda98257b01ca63195e417b0
TakeADev <chanceconwaydev@gmail.com>
2023-12-15 16:47:23 -0500

    Photos will be shown in order from newest to oldest

--- src/components/Profile/ProfilePhotosTab.tsx
+++ src/components/Profile/ProfilePhotosTab.tsx
@@ -7,18 +7,22 @@ interface IProfilePhotosTabProps {
 }
 
 const ProfilePhotosTab: FunctionComponent<IProfilePhotosTabProps> = ({ profileUserDoc }) => {
+  const photosReversed = [...profileUserDoc.photos].reverse()
+
+  if (photosReversed && photosReversed.length > 0) {
+    return (
+      <div className='grid grid-cols-3 auto-rows-fr gap-2 max-h-[500px]'>
+        {photosReversed.map((photo, index) => (
+          <div className='hover:scale-[101%] hover:cursor-pointer' key={index}>
+            <img src={photo} className='h-full w-full object-cover' />
+          </div>
+        ))}
+      </div>
+    )
+  }
   return (
-    <div className='grid grid-cols-3 auto-rows-fr gap-2 '>
-      {profileUserDoc.photos &&
-        profileUserDoc.photos.map((photo, index) => {
-          return (
-            <>
-              <div className='hover:scale-[101%] hover:cursor-pointer' key={index}>
-                <img src={photo} className='h-full w-full object-cover' />
-              </div>
-            </>
-          )
-        })}
+    <div className='text-center text-3xl mt-10 text-gray-400'>
+      <span>This user has not uploaded any photos yet.</span>
     </div>
   )
 }
