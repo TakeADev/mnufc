9b5e501e71b09d0ac7b459168e3ade4252695108
TakeADev <chanceconwaydev@gmail.com>
2023-12-15 16:45:02 -0500

    Add icon and feature to add photos to posts

--- src/components/CreatePost/CreatePost.tsx
+++ src/components/CreatePost/CreatePost.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, FunctionComponent } from 'react'
+import { useState, useContext, FunctionComponent, useRef } from 'react'
 
 import { useParams } from 'react-router-dom'
 
@@ -6,12 +6,15 @@ import Button from '../Button'
 import { UserContext } from '../../contexts/User'
 import { IUserPost, UserPostsContext } from '../../contexts/UserPosts'
 import { ModalContext } from '../../contexts/ModalContext'
+import { MdClose, MdOutlinePhoto } from 'react-icons/md'
+import { uploadUserPhoto } from '../../utils/firebase/firebase-config'
 
 interface ICreatePostProps {
   isReply?: boolean
   replyModalPost?: IUserPost
   addedClasses?: string
   autoFocus?: boolean
+  attachedPhoto?: string
 }
 
 const CreatePost: FunctionComponent<ICreatePostProps> = ({
@@ -21,8 +24,9 @@ const CreatePost: FunctionComponent<ICreatePostProps> = ({
   autoFocus,
 }) => {
   const [postData, setPostData] = useState('')
+  const [attachedPhoto, setAttachedPhoto] = useState(null)
 
-  const { currentAuthUser } = useContext(UserContext)
+  const { currentAuthUser, currentUserDoc } = useContext(UserContext)
   const { createNewUserPost } = useContext(UserPostsContext)
   const { setModalIsOpen } = useContext(ModalContext)
 
@@ -47,10 +51,16 @@ const CreatePost: FunctionComponent<ICreatePostProps> = ({
 
   const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement> | React.KeyboardEvent) => {
     e.preventDefault()
-    if (postData) {
+    if (postData || attachedPhoto) {
       resetFormValue()
-      createNewUserPost(currentAuthUser, postData, replyTo)
+      if (attachedPhoto) {
+        await uploadUserPhoto(attachedPhoto, currentUserDoc).then((res) =>
+          createNewUserPost(currentAuthUser, postData, replyTo, res)
+        )
+        setAttachedPhoto(null)
+      } else createNewUserPost(currentAuthUser, postData, replyTo)
     }
+    setAttachedPhoto(null)
     setModalIsOpen(false)
   }
 
@@ -61,9 +71,28 @@ const CreatePost: FunctionComponent<ICreatePostProps> = ({
     }
   }
 
+  const inputPhoto = useRef(null)
+
+  const inputPhotoClickHandler = () => {
+    inputPhoto.current.click()
+  }
+
+  const inputPhotoChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader()
+
+    reader.readAsDataURL(e.target.files[0])
+    reader.onload = (e) => {
+      setAttachedPhoto(e.target.result)
+    }
+  }
+
+  const attachedPhotoCloseClickHandler = () => {
+    setAttachedPhoto(null)
+  }
+
   return (
     <div
-      className={`pb-3 pt-5 mx-auto px-3 ${
+      className={`pb-5 pt-5 mx-auto px-3 ${
         //Adds styling only if not in reply modal
         !replyModalPost && 'border-slate-700 border-l border-r border-b'
       } ${addedClasses}`}
@@ -80,8 +109,30 @@ const CreatePost: FunctionComponent<ICreatePostProps> = ({
           onChange={onChangeHandler}
           onKeyDown={postEnterSubmit}
           placeholder={isReply ? `Write a reply...` : `Write a post...`}
-          className='bg-slate-800 focus:ring-0 focus:border focus:border-cyan-600 focus:outline-none p-3 rounded-lg resize-none w-full'
+          className='bg-slate-950 focus:ring-0  focus:outline-none p-3 rounded-lg resize-none w-full'
         />
+        {attachedPhoto && (
+          <div className='relative'>
+            <MdClose
+              className='text-3xl bg-opacity-70 absolute bg-gray-700 rounded-full p-1 m-3 cursor-pointer'
+              onClick={attachedPhotoCloseClickHandler}
+            />
+            <img src={attachedPhoto} className='rounded-lg mb-0 max-h-[500px]' />
+          </div>
+        )}
+        <div className='mt-5'>
+          <div className='absolute ml-3 -mt-1 text-3xl text-cyan-500 rounded-lg hover:bg-gray-700 hover:cursor-pointer'>
+            <MdOutlinePhoto className='' onClick={inputPhotoClickHandler} />
+            <input
+              type='file'
+              id='inputPhoto'
+              ref={inputPhoto}
+              accept='.png, .jpg, .jpeg'
+              className='hidden'
+              onChange={inputPhotoChangeHandler}
+            />
+          </div>
+        </div>
         <div className='text-right mt-2'>
           <Button type='submit' addedClasses='w-20'>
             Post
