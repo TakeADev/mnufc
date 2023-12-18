8325783a5f9b7b51cd7503cd2bf15c742d366f7d
TakeADev <chanceconwaydev@gmail.com>
2023-12-18 14:49:49 -0500

    Include flash message

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Modal from '../components/Modal/Modal'
+import FlashMessage from '../components/FlashMessage/FlashMessage'
 
 function Navigation() {
   return (
     <>
+      <FlashMessage />
       <Modal>
         <NavBar />
         <Outlet />
