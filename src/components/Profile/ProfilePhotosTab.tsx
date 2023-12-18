6868b9298ddad54fa447bed4ad1a59aee8e39877
TakeADev <chanceconwaydev@gmail.com>
2023-12-18 11:22:13 -0500

    Photos recieved from posts rather than userDoc

--- src/components/Profile/ProfilePhotosTab.tsx
+++ src/components/Profile/ProfilePhotosTab.tsx
@@ -1,25 +1,46 @@
-import { FunctionComponent } from 'react'
+import { useContext, FunctionComponent } from 'react'
+
+import { useNavigate, useParams } from 'react-router'
+
+import { UserPostsContext } from '../../contexts/UserPosts'
 
 import { IUserDoc } from '../../contexts/User'
+import { IUserPost } from '../../contexts/UserPosts'
 
 interface IProfilePhotosTabProps {
   profileUserDoc: IUserDoc
 }
 
 const ProfilePhotosTab: FunctionComponent<IProfilePhotosTabProps> = ({ profileUserDoc }) => {
-  const photosReversed = [...profileUserDoc.photos].reverse()
+  const { userPosts } = useContext(UserPostsContext)
 
-  if (photosReversed && photosReversed.length > 0) {
+  const navigate = useNavigate()
+  let photoPosts = []
+
+  if (userPosts) {
+    userPosts.map((post) => {
+      if (post.uid === profileUserDoc.uid) {
+        post.attachedPhoto && photoPosts.push(post)
+      }
+    })
+  }
+
+  if (photoPosts) {
     return (
       <div className='grid grid-cols-3 auto-rows-fr gap-2 max-h-[500px]'>
-        {photosReversed.map((photo, index) => (
-          <div className='hover:scale-[101%] hover:cursor-pointer' key={index}>
-            <img src={photo} className='h-full w-full object-cover' />
+        {photoPosts.map((post, index) => (
+          <div
+            className='hover:scale-[101%] hover:cursor-pointer'
+            key={index}
+            onClick={() => navigate(`/${post.username}/status/${post.postId}/photo`)}
+          >
+            <img src={post.attachedPhoto} className='h-full w-full object-cover' />
           </div>
         ))}
       </div>
     )
   }
+
   return (
     <div className='text-center text-3xl mt-10 text-gray-400'>
       <span>This user has not uploaded any photos yet.</span>
