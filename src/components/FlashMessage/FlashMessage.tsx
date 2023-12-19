657553a94c8a2bd2e11ee13d8371f699ff8ff270
TakeADev <chanceconwaydev@gmail.com>
2023-12-19 11:56:21 -0500

    Destructure flash types

--- src/components/FlashMessage/FlashMessage.tsx
+++ src/components/FlashMessage/FlashMessage.tsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react'
 
 import { FlashMessageContext } from '../../contexts/FlashMessageContext'
+
 import { FLASH_TYPES } from '../../contexts/FlashMessageContext'
+const { copy } = FLASH_TYPES
 
 import '../../styles/animations.scss'
 
@@ -9,7 +11,7 @@ const FlashMessage = () => {
   const { flashType } = useContext(FlashMessageContext)
 
   switch (flashType) {
-    case FLASH_TYPES.copy:
+    case copy:
       return (
         <div className='absolute z-50 w-full min-h-screen flex justify-center pointer-events-none'>
           <div className='fixed top-36 bg-orange-300 text-center px-10 text-xl text-orange-700 fade-in-and-out rounded-lg py-2'>
