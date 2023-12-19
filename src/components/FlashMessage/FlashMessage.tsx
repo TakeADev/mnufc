2c8dc116417d3fb0d6e67934a69f358dac6a9976
TakeADev <chanceconwaydev@gmail.com>
2023-12-19 11:54:31 -0500

    Change text size of flash message.

--- src/components/FlashMessage/FlashMessage.tsx
+++ src/components/FlashMessage/FlashMessage.tsx
@@ -12,7 +12,7 @@ const FlashMessage = () => {
     case FLASH_TYPES.copy:
       return (
         <div className='absolute z-50 w-full min-h-screen flex justify-center pointer-events-none'>
-          <div className='fixed top-36 bg-orange-300 text-center px-10 text-2xl text-orange-700 fade-in-and-out rounded-lg py-2'>
+          <div className='fixed top-36 bg-orange-300 text-center px-10 text-xl text-orange-700 fade-in-and-out rounded-lg py-2'>
             <span className=''>Link has been coppied to your clipboard</span>
           </div>
         </div>
