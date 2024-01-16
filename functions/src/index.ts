de210b321d03aa3a5dda7dece83cc70fde3e1c8b
TakeADev <chanceconwaydev@gmail.com>
2024-01-16 11:16:36 -0500

    Setup web scraper cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,40 +1,39 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const { logger } = require('firebase-functions')
 const { onRequest } = require('firebase-functions/v2/https')
-const { onDocumentCreated } = require('firebase-functions/v2/firestore')
-
-// The Firebase Admin SDK to access Firestore.
 const { initializeApp } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
+const { logger } = require('firebase-functions')
 
+const puppeteer = require('puppeteer')
 initializeApp()
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addmessage = onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await getFirestore().collection('messages').add({ original: original })
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${writeResult.id} added.` })
-})
+exports.scrapeTwitter = onRequest({ memory: '2GiB', timeoutSeconds: 300 }, async (req, res) => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+  })
+  const page = await browser.newPage()
 
-// Listens for new messages added to /messages/:documentId/original
-// and saves an uppercased version of the message
-// to /messages/:documentId/uppercase
-exports.makeuppercase = onDocumentCreated('/messages/{documentId}', (event) => {
-  // Grab the current value of what was written to Firestore.
-  const original = event.data.data().original
+  await page.goto('https://nitter.net/MNUFC')
 
-  // Access the parameter `{documentId}` with `event.params`
-  logger.log('Uppercasing', event.params.documentId, original)
+  const tweets = await page.$$eval('.timeline-item', (els) => {
+    return els.map((e) => {
+      const tweetContent = e.querySelector('.tweet-content').innerHTML
+      const tweetAttachments = Array.from(e.querySelectorAll('.still-image'), (e) => e.toString())
+      const tweetTimestamp = e.querySelector('.tweet-date').firstChild.getAttribute('title')
 
-  const uppercase = original.toUpperCase()
+      const timestampRe = /(?!UTC)\b(\w:?)+/
+      console.log(tweetTimestamp)
 
-  // You must return a Promise when performing
-  // asynchronous tasks inside a function
-  // such as writing to Firestore.
-  // Setting an 'uppercase' field in Firestore document returns a Promise.
-  return event.data.ref.set({ uppercase }, { merge: true })
+      return {
+        tweetContent: tweetContent,
+        tweetAttachments: tweetAttachments,
+        tweetTimestamp: tweetTimestamp,
+      }
+    })
+  })
+
+  console.log(tweets)
+
+  res.send('done')
+
+  await browser.close()
 })
